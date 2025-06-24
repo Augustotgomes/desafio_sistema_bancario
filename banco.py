b931d61ce802d47f13abe26bf675379bfccc7b18
Augusto Gomes <augusto.tgomes@gmail.com>
2025-06-23 23:58:20 -0300

    desafio sistema banco

--- banco.py
+++ banco.py
@@ -24,8 +24,9 @@ extratos += "saque de 500 reais \n"
 while True:
     
     opcao = input(menu)
+    print(opcao)
 
-    if opcao == 1:
+    if opcao == "1":
         valor = float(input("Digite o valor a ser depositado: "))
 
         if valor > 0:
@@ -34,9 +35,35 @@ while True:
         
         else: 
             print("Operação falhou! O valor informado é inválido, digite um valor maior que Zero")
-            
-    elif opcao == "q":
+
+    
+    elif opcao == "2":
+        valor = float(input("Digite o valor a ser sacado: "))
+
+        if valor > saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        
+        elif valor > limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Operação falhou! Você já realizou o número máximo de saques.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque de R$ {valor:.2f} reais \n"
+            numero_saques += 1
+        
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "4":
         break
 
-
-
+    else:
+      print("digite uma opção válida")
